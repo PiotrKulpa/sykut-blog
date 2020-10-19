e1f423cf0b3caee19ae9a946093be97b3c3a9218
unknown <info.kulpa@gmail.com>
2020-10-19 21:34:02 +0200

    refactor search wip

--- pages/szukaj.js
+++ pages/szukaj.js
@@ -8,6 +8,8 @@ import parseMD from 'parse-md';
 
 import PostsWrapper from '../components/PostsWrapper';
 import SearchPosts from '../components/SearchPosts';
+import { BLOG_FILES_PATH } from '../constants';
+import getParsedPosts from '../helpers/getParsedPosts';
 
 const Search = ({ posts = [] }) => {
   return (
@@ -17,45 +19,55 @@ const Search = ({ posts = [] }) => {
   )
 }
 
-export const getStaticProps = async () => {
-  const files = fs.readdirSync("content/blog");
+// export const getStaticProps = async () => {
+//   const files = fs.readdirSync("content/blog");
 
+//   return {
+//     props: {
+//       posts: files && files.map(filename => {
+//         const markdownWithMetadata = fs
+//           .readFileSync(path.join("content/blog", filename), 'utf8')
+//         const {
+//           metadata: {
+//             date = '',
+//             title = '',
+//             tags = '',
+//             featuredImage = '',
+//             content = '',
+//           },
+//         } = parseMD(markdownWithMetadata);
+
+//         const parsedMarkdown = matter(content);
+//         const htmlString = marked(parsedMarkdown.content);
+
+//         return {
+//           slug: filename.replace(".md", ""),
+//           date: date.toString(),
+//           title,
+//           featuredImage,
+//           tags,
+//           htmlString,
+//         }
+
+//       })
+//         .sort((a, b) => {
+//           var dateA = new Date(a.date);
+//           var dateB = new Date(b.date);
+//           return dateA - dateB;
+//         })
+//         .reverse()
+//     }
+//   };
+// };
+
+export const getServerSideProps = async (context) => {
+  const files = fs.readdirSync(BLOG_FILES_PATH);
   return {
     props: {
-      posts: files && files.map(filename => {
-        const markdownWithMetadata = fs
-          .readFileSync(path.join("content/blog", filename), 'utf8')
-        const {
-          metadata: {
-            date = '',
-            title = '',
-            tags = '',
-            featuredImage = '',
-            content = '',
-          },
-        } = parseMD(markdownWithMetadata);
+      posts: getParsedPosts(files),
 
-        const parsedMarkdown = matter(content);
-        const htmlString = marked(parsedMarkdown.content);
-
-        return {
-          slug: filename.replace(".md", ""),
-          date: date.toString(),
-          title,
-          featuredImage,
-          tags,
-          htmlString,
-        }
-
-      })
-        .sort((a, b) => {
-          var dateA = new Date(a.date);
-          var dateB = new Date(b.date);
-          return dateA - dateB;
-        })
-        .reverse()
-    }
-  };
-};
+    }, // will be passed to the page component as props
+  }
+}
 
 export default Search;
