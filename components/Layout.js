24f9f55742e6551ec27dcdd7f6d1b2cf2cfbcc61
unknown <info.kulpa@gmail.com>
2020-10-28 20:55:32 +0100

    add cookie consent

--- components/Layout.js
+++ components/Layout.js
@@ -2,22 +2,32 @@ import React, { Component } from 'react';
 import Head from "next/head";
 import Header from './Header';
 import Footer from './Footer';
-import Breadcrumbs from './Breadcrumbs';
+import CookieConsent from "react-cookie-consent";
 
 class Layout extends Component {
   render() {
     const { children } = this.props
     return (
       <>
-      <Head>
-      <title>Dr N. Med Janusz Sykut</title>
-      </Head>
-      <div id="wrapper" className="wrapper">
-        <Header />
-        {/* <Breadcrumbs /> */}
-        {children}
-        <Footer />
-      </div>
+        <Head>
+          <title>Dr N. Med Janusz Sykut</title>
+        </Head>
+        <div id="wrapper" className="wrapper">
+          <Header />
+          {children}
+          <Footer />
+          <CookieConsent
+            location="bottom"
+            buttonText="Zgoda."
+            cookieName="myAwesomeCookieName2"
+            style={{ background: "#F8F8F8", color: "black" }}
+            buttonStyle={{ color: "#4e503b", fontSize: "13px" }}
+            expires={150}
+          >
+            Ta strona korzysta z ciasteczek, aby świadczyć usługi na najwyższym poziomie. 
+            Dalsze korzystanie ze strony oznacza, że zgadzasz się na ich użycie.
+          </CookieConsent>
+        </div>
       </>
     );
   }
