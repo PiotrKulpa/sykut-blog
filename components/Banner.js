c9b707b6a86e114a9b45cbfe09c19afa5f6377c6
unknown <info.kulpa@gmail.com>
2020-10-28 21:54:04 +0100

    fix banner wip

--- components/Banner.js
+++ components/Banner.js
@@ -11,26 +11,6 @@ const Banner = () => {
   const { homeMainSlider = [] } = attributes || {};
 
   return (
-    <section className="slider-wrap-layout1 box-layout-child-4">
-      {/* <div className="slider-left-side-content">DR JANUSZ SYKUT BLOG</div> */}
-      {/* <div className="slider-right-side-content">
-        <ul>
-          <li>
-            <a href="https://www.instagram.com/challenge/?next=/drnmed_jsykut/%253Fhl%253Dpl">
-              <i className="fab fa-instagram"></i>
-            </a>
-          </li>
-          <li>
-            <a href="https://pl.linkedin.com/">
-              <i className="fab fa-linkedin"></i>
-            </a>
-          </li>
-        </ul>
-      </div> */}
-      <div className="full-width-container" style={{ width: "100%" }}>
-        <div className="slider-area slider-layout1 slider-top-margin95">
-          <div className="bend niceties preview-1">
-            <div className="ensign-nivoslider-1"></div>
             <AutoplaySlider
               play={false}
               cancelOnInteraction={false}
@@ -47,30 +27,24 @@ const Banner = () => {
                 title = ''
               }, i) =>
 
-                <div style={{
-                  backgroundImage: `url(${imgUrl})`,
-                  }} key={i}>
+                <div key={i}>
 
                   <div className="custom-banner-content">
-                    <div>Left</div>
-                    <div>
+                    <div className="custom-banner-content-left">Left</div>
+                    <div className="custom-banner-middle">
                       <p className="custom-slider-title">{title}</p>
                       <p className="custom-slider-sub-text">{subtitle}</p>
                       <p className="custom-slider-content">{content}</p>
                     </div>
-                    <div>Right</div>
+                    <div className="custom-banner-content-right">Right</div>
                   </div>
 
-                  {/* <img src={imgUrl} alt="" /> */}
+                  <img src={imgUrl} alt="" />
                 </div>
 
               )}
             </AutoplaySlider>
-          </div>
-        </div>
-      </div>
 
-    </section >
   );
 
 }
