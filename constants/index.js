ad9117245d3815a42974d028ad2c3afd0c6bcd71
unknown <info.kulpa@gmail.com>
2021-01-23 07:43:37 +0100

    6 posts per page

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const DEFAULT_POSTS_PER_PAGE = 5;
+export const DEFAULT_POSTS_PER_PAGE = 6;
 export const DEFAULT_NUMBER_OF_PAGES = 3;
 export const BLOG_FILES_PATH = 'content/blog';
 export const POST_URL_PATH = '/blog/wpis/';
